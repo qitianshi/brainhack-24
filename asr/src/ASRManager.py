a8090cd2145fc091827c4414c49e6491fd55d602
qitianshi <65122416+qitianshi@users.noreply.github.com>
2024-05-31 19:28:34 +0000

    Remove redundant ASRManager code

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -17,31 +17,6 @@ class ASRManager:
         
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model.to(self.device)
-        
-    @staticmethod
-    def correct_heading(text):
-    
-        # This is some stupid hardcoded version because the real one is overkill here.
-        # The actual methodology is to extract the heading, find words that don't match
-        # the ten number words, get their pronunciation from CMUdict, calculate their
-        # Levenshtein distance to our expected words, and see which one is the closest.
-
-        # Define the pattern to match
-        match = re.compile(r'heading is(.*?)(?:tool to deploy is|target is|$)', re.IGNORECASE).search(text)
-
-        # If no match, raise an error.
-        if not match:
-            raise ValueError("No heading found.")
-
-        heading = match.group(1)
-
-        # hardcoded because 8-)
-        # this is our only error rn
-        heading = heading.replace("by", "five")
-
-        result = text[:match.start(1)] + heading + text[match.end(1):]
-
-        return result
 
     def transcribe(self, audio_bytes: bytes) -> str:
         """perform ASR transcription on one audio represented as audio bytes"""
